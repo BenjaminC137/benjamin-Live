536e703586e0bcc974507efd01944dfa03fedefa
Benjamin <40469901+BenjaminC137@users.noreply.github.com>
2018-10-18 15:51:28 -0500

    added more model properties and declared them
    and updated look and feel. added most content to projects

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<app-nav-bar></app-nav-bar>\n<br><br>\n<app-arduino></app-arduino>\n"
+module.exports = "<app-nav-bar></app-nav-bar>\n<br><br>\n<app-arduino></app-arduino>\n<br>\n© 2018 Benjamin Salasek\n"
 
 /***/ }),
 
@@ -159,7 +159,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!--\n<div *ngIf=\"arduino\">\n  <h2>{{arduino.title | uppercase}} Details</h2>\n  <div><span>id: </span>{{arduino.id}}</div>\n  <div>\n    <label>title:\n      <input [(ngModel)]=\"arduino.title\" placeholder=\"title\"/>\n    </label>\n  </div>\n-->\n<div class=\"project\">\n\t<h3>{{arduino.title | uppercase}}</h3>\n\t<br>\n\t<p>{{arduino.description}}\n\t<br>\n\tCreated: {{arduino.dateCreated | date}}\n\t<br>\n\tCompleted: {{arduino.dateCompleted | date}}\n\t<br>\n\t\t</p>\n<div class=\"\">\n\t\t<a href=\"{{arduino.imagePath}}\"><img class=\"arduino-cards img-scale\" src=\"{{arduino.imagePath}}\"></a>\n\t</div>\n\t<br>\n\n\t<h3>More images: </h3>\n<div class=\"container\">\n\t<div class=\"arduino-cards\" *ngFor=\"let image of arduino.moreImagesPath\"><a href=\"{{image}}\"><img class=\"img-scale\" src={{image}}></a></div>\n</div>\n<!--\t<div *ngFor=\"let arduino of \" ></div>-->\n\n<!--\n\t\t<div class=\"card rounded-image arduino-cards\"\n\t\t*ngFor=\"let arduino of arduinoProjects\"     [class.selected]=\"arduino === selectedArduino\" (click)=\"onSelect(arduino)\"><h6>{{arduino.title}}</h6>\n\t\t<img src=\"{{arduino.imagePath}}\">\n\t</div>\n-->\n</div>\n"
+module.exports = "<!--\n<div *ngIf=\"arduino\">\n  <h2>{{arduino.title | uppercase}} Details</h2>\n  <div><span>id: </span>{{arduino.id}}</div>\n  <div>\n    <label>title:\n      <input [(ngModel)]=\"arduino.title\" placeholder=\"title\"/>\n    </label>\n  </div>\n-->\n<div class=\"project\">\n\t<h3>{{arduino.title | uppercase}}</h3>\n\t<br>\n\t<div class=\"box\">\n\t\t<h4>Introduction: </h4>\n\t\t{{arduino.description}}\n\t\t<br>\n\t\t<br>\n\t\tCreated: {{arduino.dateCreated | date}}\n\t\t<br>\n\t\tCompleted: {{arduino.dateCompleted | date}}\n\t\t<br>\n\t</div>\n\t<br>\n\t<div class=\"box\">\n\t\t<h4>Components: </h4>\n\t\t<ul>\n\t\t\t<li *ngFor=\"let component of arduino.components\">{{component}}</li>\n\t\t</ul>\n\t</div>\n\t<br>\n\t<div class=\"box\">\n\t\t<h4>Libraries: </h4>\n\t\t<ul>\n\t\t\t<li *ngFor=\"let library of arduino.libraries\">{{library}}</li>\n\t\t</ul>\n\t</div>\n\t<br>\n\t<div class=\"arduino-cards\">\n\t\t<img class=\"img-scale\" src=\"{{arduino.imagePath}}\">\n\t\t<a href=\"{{arduino.imagePath}}\">View hi-res\n\t\t</a>\n\t</div>\n\t<br>\n<!--\n\t<div class=\"box\"><iframe class=\"box\" width=\"560\" height=\"315\" src=\"{{arduino.videoUrl}}\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe></div>\n-->\n\t<h4>More images: </h4>\n\t<div class=\"container\">\n\t\t<div class=\"row\">\n\t\t\t<div class=\"col arduino-cards\" *ngFor=\"let image of arduino.moreImagesPath\">\n\t\t\t\t<img class=\"img-scale\" src={{image}}>\n\t\t\t\t<a href=\"{{image}}\">View hi-res</a>\n\t\t\t</div>\n\t\t</div>\n\t</div>\n\t\t<h4>Video: </h4>\n\t\t<p>{{arduino.videoUrl}}</p>\n</div>\n<br>\n<br>\n"
 
 /***/ }),
 
@@ -248,19 +248,25 @@ var ArduinoProjects = [
         dateCreated: '2018/03/21',
         dateCompleted: '2018/04/18',
         imagePath: "assets/ArduinoNeechiFeeder/Neechi-full-IMG_8278.png",
-        moreImagesPath: ["assets/ArduinoNeechiFeeder/Neechi-agitator-assembly-IMG_8224.png", "assets/ArduinoNeechiFeeder/Neechi-agitator-rod-IMG_8261-2.png", "assets/ArduinoNeechiFeeder/Neechi-agitator-assembly-IMG_8224.png",
-            "assets/ArduinoNeechiFeeder/Neechi-agitator-rod-IMG_8261-2.png",
-            "assets/ArduinoNeechiFeeder/Neechi-door-not-cut-IMG_6596.png",
-            "assets/ArduinoNeechiFeeder/Neechi-electronics-and-machine-IMG_7498.png",
-            "assets/ArduinoNeechiFeeder/Neechi-electronics-dark-IMG_7545.png",
-            "assets/ArduinoNeechiFeeder/Neechi-full-room-IMG_8273.png",
-            "assets/ArduinoNeechiFeeder/Neechi-motor-mount-back-IMG_8192.png",
-            "assets/ArduinoNeechiFeeder/Neechi-motor-mount-IMG_8181.png",
-            "assets/ArduinoNeechiFeeder/Neechi-motor-mount-on-motor-IMG_8174.png",
-            "assets/ArduinoNeechiFeeder/Neechi-motor-mounted-IMG_6585.png",
-            "assets/ArduinoNeechiFeeder/Neechi-mounted-old-stepper-IMG_7465.png",
-            "assets/ArduinoNeechiFeeder/Neechi-room-IMG_8230.png",
-            "assets/ArduinoNeechiFeeder/Neechi-workbench-IMG_7441.png"
+        moreImagesPath: [
+            "assets/ArduinoNeechiFeeder/Neechi-agitator-assembly-IMG_8224.png", "assets/ArduinoNeechiFeeder/Neechi-agitator-rod-IMG_8261-2.png", "assets/ArduinoNeechiFeeder/Neechi-agitator-assembly-IMG_8224.png", "assets/ArduinoNeechiFeeder/Neechi-agitator-rod-IMG_8261-2.png", "assets/ArduinoNeechiFeeder/Neechi-door-not-cut-IMG_6596.png", "assets/ArduinoNeechiFeeder/Neechi-electronics-and-machine-IMG_7498.png", "assets/ArduinoNeechiFeeder/Neechi-electronics-dark-IMG_7545.png", "assets/ArduinoNeechiFeeder/Neechi-full-room-IMG_8273.png", "assets/ArduinoNeechiFeeder/Neechi-motor-mount-back-IMG_8192.png", "assets/ArduinoNeechiFeeder/Neechi-motor-mount-IMG_8181.png", "assets/ArduinoNeechiFeeder/Neechi-motor-mount-on-motor-IMG_8174.png", "assets/ArduinoNeechiFeeder/Neechi-motor-mounted-IMG_6585.png", "assets/ArduinoNeechiFeeder/Neechi-mounted-old-stepper-IMG_7465.png", "assets/ArduinoNeechiFeeder/Neechi-room-IMG_8230.png", "assets/ArduinoNeechiFeeder/Neechi-workbench-IMG_7441.png"
+        ],
+        videoUrl: "https://www.youtube.com/embed/lyI5C47GDH0",
+        components: [
+            "Arduino nano",
+            "IIC/I2C/TWI 1602 Serial LCD Display",
+            "IIC/I2C/TWI 1602 Serial Interface module",
+            "servo motor",
+            "stepper motor"
+        ],
+        libraries: [
+            "DS1302RTC.h",
+            "Time.h",
+            "Wire.h",
+            "Servo.h",
+            "LCD.h",
+            "LiquidCrystal_I2C.h",
+            "Stepper.h"
         ]
     },
     {
@@ -269,7 +275,23 @@ var ArduinoProjects = [
         description: 'A car that roams around the house for companionship so people don\'t feel lonely.',
         dateCreated: '2016/01/01',
         dateCompleted: '2016/01/02',
-        imagePath: "assets/ArduinoBuddy/buddy-full-IMG_0423.png"
+        imagePath: "assets/ArduinoBuddy/buddy-full-IMG_0423.png",
+        moreImagesPath: [
+            "assets/coming-soon.png"
+        ],
+        videoUrl: "coming soon",
+        components: [
+            "Arduino nano",
+            "Ultrasonic sensor",
+            "car chasis",
+            "DC motor",
+            "wheel",
+            "accelerometer",
+            "led"
+        ],
+        libraries: [
+            "none"
+        ]
     },
     {
         id: 3,
@@ -277,7 +299,24 @@ var ArduinoProjects = [
         description: 'Counts crunches while I exercise so I can watch TV and not have to count. I lose track when I count anyway because I start thinking about other things.',
         dateCreated: '2018/10/05',
         dateCompleted: '2018/10/08',
-        imagePath: "assets/ArduinoCrunchCounter/crunchCounter-beta-IMG_4616.jpg"
+        imagePath: "assets/ArduinoCrunchCounter/crunchCounter-beta-IMG_4616.jpg",
+        moreImagesPath: [
+            "assets/coming-soon.png"
+        ],
+        videoUrl: "cooming soon",
+        components: [
+            "Arduino nano",
+            "photoresistor",
+            "led",
+            "IIC/I2C/TWI 1602 Serial LCD Display",
+            "IIC/I2C/TWI 1602 Serial Interface module",
+            "buzzer"
+        ],
+        libraries: [
+            "Wire.h",
+            "LCD.h",
+            "LiquidCrystal_I2C.h"
+        ]
     },
     {
         id: 4,
@@ -285,7 +324,20 @@ var ArduinoProjects = [
         description: 'Box on fridge buzzes and flashes when open too long or it doesn\'t fully close.',
         dateCreated: '2017/01/01',
         dateCompleted: '2017/01/01',
-        imagePath: "assets/ArduinoFridgeAlarm/FridgeAlarm-box-IMG_4612.jpg"
+        imagePath: "assets/ArduinoFridgeAlarm/FridgeAlarm-box-IMG_4612.jpg",
+        moreImagesPath: [
+            "assets/ArduinoFridgeAlarm/FridgeAlarm-button-IMG_4614.jpg"
+        ],
+        videoUrl: "coming soon",
+        components: [
+            "Arduino nano",
+            "button",
+            "led",
+            "buzzer"
+        ],
+        libraries: [
+            "none"
+        ]
     },
     {
         id: 5,
@@ -293,7 +345,20 @@ var ArduinoProjects = [
         description: 'Automatically turns on lights when you walk by. Uses the unused buttons on the TV remote to turn on or off. Has manual buttons.',
         dateCreated: '2017/01/01',
         dateCompleted: '2017/01/01',
-        imagePath: "assets/ArduinoHomeBox/Homebox-complete-IMG_8136.png"
+        imagePath: "assets/ArduinoHomeBox/Homebox-complete-IMG_8136.png",
+        moreImagesPath: [
+            "assets/coming-soon.png"
+        ],
+        videoUrl: "coming soon",
+        components: [
+            "Arduino nano",
+            "button",
+            "IR receiver",
+            "5V relay module"
+        ],
+        libraries: [
+            "IRremote.h"
+        ]
     },
     {
         id: 6,
@@ -301,7 +366,31 @@ var ArduinoProjects = [
         description: 'Analyzes surroundings and chooses where to go.',
         dateCreated: '2017/01/01',
         dateCompleted: '2017/01/01',
-        imagePath: "assets/ArduinoSmartTank/smartTank-hot-art-IMG_3017.jpeg"
+        imagePath: "assets/ArduinoSmartTank/smartTank-hot-art-IMG_3017.jpeg",
+        moreImagesPath: [
+            "assets/coming-soon.png"
+        ],
+        videoUrl: "coming soon",
+        components: [
+            "Arduino nano",
+            "DC motor",
+            "led",
+            "RF receiver",
+            "RF transmitter",
+            "button",
+            "led",
+            "servo motor",
+            "ultrasonic sensor",
+            "wheel",
+            "treads",
+            "triangle vehicle chasis",
+            "potentiometer"
+        ],
+        libraries: [
+            "SPI.h",
+            "RH_ASK.h",
+            "Servo.h",
+        ]
     },
     {
         id: 7,
@@ -309,7 +398,19 @@ var ArduinoProjects = [
         description: 'Security System to fend off intruders.',
         dateCreated: '2017/01/01',
         dateCompleted: '2017/01/01',
-        imagePath: "assets/ArduinoSecuritySystem/SecuritySystem-inside-IMG_4506.jpeg"
+        imagePath: "assets/ArduinoSecuritySystem/SecuritySystem-inside-IMG_4506.jpeg",
+        moreImagesPath: [
+            "assets/coming-soon.png"
+        ],
+        videoUrl: "coming soon",
+        components: [
+            "Arduino nano",
+            "common cathode RGB led",
+            "PIR proximety sensor"
+        ],
+        libraries: [
+            "none"
+        ]
     },
     {
         id: 8,
@@ -317,7 +418,33 @@ var ArduinoProjects = [
         description: 'Modified a space heater to be able to turn on at given time.',
         dateCreated: '2017/01/01',
         dateCompleted: '2017/01/01',
-        imagePath: "assets/ArduinoHeaterControl/ArduinoHeaterControl-BoardAndCables1%20-%20IMG_2368%20copy.jpeg"
+        imagePath: "assets/ArduinoHeaterControl/ArduinoHeaterControl-BoardAndCables1%20-%20IMG_2368%20copy.jpeg",
+        moreImagesPath: [
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-board-bottom-partial - IMG_2343 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-board-bottom-partial-withBackground - IMG_2342 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-board-top - IMG_2363 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-BoardAndCables1 - IMG_2368 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-BoardAndCables2 - IMG_2367 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-BoardComplete - IMG_2366 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-BoardSide - IMG_2365 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-breadboardPrep - IMG_1757 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-Cable-entry-internal - IMG_1741 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-cableExit - IMG_1742 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-CableExit2 - IMG_1754 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-CablesSolderedToController1 - IMG_1747 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-CablesSolderedToController2 - IMG_1753 copy.jpeg",
+            "assets/ArduinoHeaterControl/ArduinoHeaterControl-Controller - IMG_1740 copy.jpeg"
+        ],
+        videoUrl: "coming soon",
+        components: [
+            "Arduino nano",
+            "led",
+            "IC 4066 quad bilateral switch",
+            "button"
+        ],
+        libraries: [
+            "none"
+        ]
     },
     {
         id: 9,
@@ -325,15 +452,50 @@ var ArduinoProjects = [
         description: 'Alarm that sounds when the temperature drops below a designated temperature to be set in the baby\s room by the crib to alert us if the temperature is too cold for the newborn.',
         dateCreated: '2017/01/01',
         dateCompleted: '2017/01/01',
-        imagePath: "assets/ArduinoTemperatureAlarm/TemperatureAlarm-Full-IMG_7909.jpeg"
+        imagePath: "assets/ArduinoTemperatureAlarm/TemperatureAlarm-Full-IMG_7909.jpeg",
+        moreImagesPath: [
+            "assets/ArduinoTemperatureAlarm/temperature-alarm-pano-IMG_7720.png",
+            "assets/ArduinoTemperatureAlarm/temperature-alarm-workspace-MG_7652.png"
+        ],
+        videoUrl: "coming soon",
+        components: [
+            "Arduino nano",
+            "button",
+            "IIC/I2C/TWI 1602 Serial LCD Display",
+            "IIC/I2C/TWI 1602 Serial Interface module"
+        ],
+        libraries: [
+            "Wire.h",
+            "LCD.h",
+            "LiquidCrystal_I2C.h"
+        ]
     },
     {
         id: 10,
         title: 'Scale',
-        description: '50 Gram scale using a .001 accuracy load cell. Includes a tare.',
+        description: '50 Gram scale using a .001 accuracy load cell. Includes a tare feature.',
         dateCreated: '2017/01/01',
         dateCompleted: '2017/01/01',
-        imagePath: "assets/ArduinoScale/scale-calibrating-IMG_4610.jpg"
+        imagePath: "assets/ArduinoScale/scale-calibrating-IMG_4610.jpg",
+        moreImagesPath: [
+            "assets/coming-soon.png"
+        ],
+        videoUrl: "coming soon",
+        components: [
+            "Arduino nano",
+            "button",
+            "HX711 Load Cell Amplifier",
+            "load cell",
+            "IIC/I2C/TWI 1602 Serial LCD Display",
+            "IIC/I2C/TWI 1602 Serial Interface module"
+        ],
+        libraries: [
+            "hx711.h",
+            "DHT.h",
+            "Wire.h",
+            "LCD.h",
+            "LiquidCrystal_I2C.h"
+        ]
     }
 ];
 
@@ -358,7 +520,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<h2> Arduino Projects</h2>\n  <div  *ngIf=\"show\">\n\t  \t<a href=\"#\" (click)=\"toggle()\" class=\"btn\">Back/Close</a>\n\t\t<app-arduino-detail [arduino]=\"selectedArduino\"></app-arduino-detail>\n   </div>\n<br>\n<div class=\"card-deck\">\n\t<div class=\"card arduino-cards\"\n\t\t*ngFor=\"let arduino of arduinoProjects\"     [class.selected]=\"arduino === selectedArduino\" (click)=\"onSelect(arduino); toggle()\"><h6>{{arduino.title}}</h6>\n\t\t<img style=\"\" src=\"{{arduino.imagePath}}\">\n\t</div>\n</div>\n"
+module.exports = "<h2> Arduino Projects</h2>\n  <div  *ngIf=\"show\">\n\t  \t<a href=\"#\" (click)=\"toggle()\" class=\"btn-primary\">Back/Close</a>\n\t\t<app-arduino-detail [arduino]=\"selectedArduino\"></app-arduino-detail>\n   </div>\n<br>\n<div class=\"card-deck\">\n\t<div class=\"card arduino-cards\"\n\t\t*ngFor=\"let arduino of arduinoProjects\"     [class.selected]=\"arduino === selectedArduino\" (click)=\"onSelect(arduino); toggle()\"><h6>{{arduino.title}}</h6>\n\t\t<img style=\"\" src=\"{{arduino.imagePath}}\">\n\t</div>\n</div>\n"
 
 /***/ }),
 
@@ -585,7 +747,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "\t<nav class=\"fixed-top navbar navbar-expand-lg navColors\">\n\t\t<a class=\"navbar-brand\" href=\"index.html\">\n\t\t\t<img class=\"navColors\" src=\"assets/rick-prison.png\" width=\"35px\" class=\"d-inline-block align-top\" alt=\"rick\" title=\"rick\"> Benjamin.Website</a>\n\t\t<button class=\"navbar-toggler custom-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarTogglerDemo02\" aria-controls=\"navbarTogglerDemo02\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n\t\t\t<span class=\"navbar-toggler-icon\"></span>\n\t\t </button>\n\n\t\t<div class=\"collapse navbar-collapse\" id=\"navbarTogglerDemo02\">\n    <ul class=\"navbar-nav mr-auto mt-2 mt-lg-0\">\n      \t<li class=\"nav-item active\">\n        <a class=\"nav-link\" href=\"index.html\">Home <span class=\"sr-only\">(current)</span></a>\n\t\t</li>\n\t\t<li class=\"nav-item\">\n        <a class=\"nav-link\" href=\"Professional.html\">Professional</a>\n      \t</li>\n\t\t<li class=\"nav-item\">\n        <a class=\"nav-link\" href=\"Personal.html\">Personal</a>\n      \t</li>\n\t\t<li class=\"nav-item\">\n        <a class=\"nav-link\" href=\"Arduino.html\">Arduino</a>\n      \t</li>\n\t\t<li class=\"nav-item\">\n        <a class=\"nav-link\" href=\"box.html\">Fireball Game!</a>\n      \t</li>\n\t\t<li class=\"nav-item\">\n        <a class=\"nav-link\" href=\"http://urldotcom.com\">urldotcom.com</a>\n      \t</li>\n\t\t<li class=\"nav-item\">\n        <a class=\"nav-link\" href=\"weather.html\">Weather</a>\n      \t</li>\n    </ul>\n  </div>\n\t</nav>\n\n"
+module.exports = "<nav class=\"fixed-top navbar navColors\">\n\t<a class=\"navbar-brand\" data-toggle=\"collapse\" data-target=\"#navbarTogglerDemo02\" aria-controls=\"navbarTogglerDemo02\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n\t\t<img class=\"navColors\" src=\"assets/PortalBenjaminDarkRes180.png\" width=\"35px\" class=\"d-inline-block align-top\" alt=\"rick\" title=\"rick\"> Benjamin.Website\n\t</a>\n\t<button class=\"navbar-toggler custom-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarTogglerDemo02\" aria-controls=\"navbarTogglerDemo02\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n\t\t<span class=\"navbar-toggler-icon\"></span>\n\t</button>\n\t<div class=\"collapse navbar-collapse\" id=\"navbarTogglerDemo02\">\n\t\t<ul class=\"navbar-nav mr-auto mt-2 mt-lg-0\">\n\t\t\t<li class=\"nav-item active\">\n\t\t\t\t<a class=\"nav-link\" href=\"index.html\">Home\n\t\t\t\t\t<span class=\"sr-only\">(current)</span>\n\t\t\t\t</a>\n\t\t\t</li>\n\t\t\t<li class=\"nav-item\">\n\t\t\t\t<a class=\"nav-link\" href=\"Professional.html\">Professional</a>\n\t\t\t</li>\n\t\t\t<li class=\"nav-item\">\n\t\t\t\t<a class=\"nav-link\" href=\"Personal.html\">Personal</a>\n\t\t\t</li>\n\t\t\t<li class=\"nav-item\">\n\t\t\t\t<a class=\"nav-link\" href=\"Arduino.html\">Arduino</a>\n\t\t\t</li>\n\t\t</ul>\n\t</div>\n</nav>\n"
 
 /***/ }),
 
